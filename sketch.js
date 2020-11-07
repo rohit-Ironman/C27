1b5b1ae60856bef7986077bd2d4e0018d4a90218
rohit-Ironman <rohit.thomas08@gmail.com>
2020-11-07 12:50:46 -0700

    constrain body

--- sketch.js
+++ sketch.js
@@ -1,6 +1,7 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
@@ -15,7 +16,7 @@ function setup(){
     engine = Engine.create();
     world = engine.world;
 
-
+    log6 = new Log(200,150,80,PI/2);
     ground = new Ground(600,height,1200,20);
     platform = new Ground(150, 305, 300, 170);
 
@@ -35,7 +36,7 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
-
+    chain = new Chain(bird.body,log6.body);
 }
 
 function draw(){
@@ -61,4 +62,7 @@ function draw(){
 
     bird.display();
     platform.display();
+
+    log6.display();
+    chain.display();
 }
